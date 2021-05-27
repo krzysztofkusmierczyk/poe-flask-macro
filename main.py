4b67e069e7875a0f33e05a319aa8d540f4e4cb96
Krzysztof Kuśmierczyk <krzysztof.kusmierczyk@outlook.com>
2021-05-27 15:22:54 +0200

    refactor a bit

--- main.py
+++ main.py
@@ -7,35 +7,37 @@ from pynput import keyboard
 
 controller = keyboard.Controller()
 
+HOTKEY = "'`'"
 MIN_DELAY = 0
 MAX_DELAY = 150
 
+# TODO: load from file
 FLASK_DURATIONS = {
-    '2': 3000,
-    '3': 6000,
-    '4': 9000,
-    '5': 1000,
+    "2": 3000,
+    "3": 6000,
+    "4": 9000,
+    "5": 1000,
 }
 
 TIMEOUT_CACHE = defaultdict(lambda: datetime.now())
 
 
 def use_flask(key: str):
-    if TIMEOUT_CACHE[key] + timedelta(milliseconds=FLASK_DURATIONS[key]) < datetime.now():
-        sleep(random.randint(MIN_DELAY, MAX_DELAY)/1000)
+    ends_at = TIMEOUT_CACHE[key] + timedelta(milliseconds=FLASK_DURATIONS[key])
+    if ends_at < datetime.now():
+        sleep(random.randint(MIN_DELAY, MAX_DELAY) / 1000)
         controller.press(key)
         TIMEOUT_CACHE[key] = datetime.now()
 
 
 def on_press(key):
-    if str(key) == "'`'":
+    if str(key) == HOTKEY:
         for key in FLASK_DURATIONS.keys():
             use_flask(key)
 
 
 def main():
-    with keyboard.Listener(
-            on_press=on_press) as listener:
+    with keyboard.Listener(on_press=on_press) as listener:
         try:
             listener.join()
         except Exception as e:
